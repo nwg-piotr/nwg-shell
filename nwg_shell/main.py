bb0154090310466128bfeb39c582eda3820b9bc4
piotr <nwg.piotr@gmail.com>
2022-05-13 03:02:04 +0200

    mention forgotten entry point

--- nwg_shell/main.py
+++ nwg_shell/main.py
@@ -6,9 +6,10 @@ from nwg_shell.__about__ import __version__
 def main():
     print("nwg-shell version {}".format(__version__))
     print("Commands:")
-    print("  nwg-shell-installer     installs selected default configs interactively")
-    print("  nwg-shell-installer -a  installs all configs from scratch")
-    print("  nwg-shell-installer -u  upgrades configs if necessary")
+    print("  nwg-shell-installer      installs selected default configs interactively")
+    print("  nwg-shell-installer -a   installs all configs from scratch")
+    print("  nwg-shell-installer -u   upgrades configs if necessary")
+    print("  nwg-shell-check-updates  checks for updates")
 
 
 if __name__ == '__main__':
