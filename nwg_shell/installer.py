25c74614f2f787feb0b6810cca456f3210c4c6f4
piotr <nwg.piotr@gmail.com>
2022-09-10 03:54:02 +0200

    fix package_data

--- nwg_shell/installer.py
+++ nwg_shell/installer.py
@@ -123,10 +123,11 @@ def main():
             copy_from_skel(item, folder="data", skip_confirmation=args.all)
 
         # copy default background
-        bcg = os.path.join(os.getenv("HOME"), ".azotebg")
+        bcg = os.path.join(os.getenv("HOME"), "azotebg")
         if not os.path.isfile(bcg):
             print("Copying default background")
-            copy(os.path.join(dir_name, "skel", "stuff", ".azotebg"), bcg)
+            copy(os.path.join(dir_name, "skel", "stuff", "azotebg"), bcg)
+            os.rename(bcg, os.path.join(os.getenv("HOME"), ".azotebg"))
 
         print("\n\nThat's all. You may run sway now.\n")
 
