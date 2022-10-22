e0af50f17a1f86f558935e03d3bd389da0b6559f
piotr <nwg.piotr@gmail.com>
2022-10-23 00:03:05 +0200

    bump to 0.3.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(f_name):
 
 setup(
     name='nwg-shell',
-    version='0.3.4',
+    version='0.3.5',
     description='GTK3-based shell for sway Wayland compositor',
     packages=find_packages(),
     include_package_data=True,
