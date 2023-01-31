87e068579737c0e9d3148f3ccbac20ea2ff4a11d
piotr <nwg.piotr@gmail.com>
2023-01-31 12:30:12 +0100

    add testing script

---
+++ scripts/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+echo "You're about to install the rest of components required for the key bindings to work."
+echo "None of them is a shell dependency, and you're free to change them later."
+
+PS3="Select file manager: "
+select fm in thunar caja dolphin nautilus nemo pcmanfm;
+do
+    break
+done
+
+PS3="Select text editor: "
+select editor in mousepad atom emacs gedit geany kate vim;
+do
+    break
+done
+
+PS3="Select web browser: "
+select browser in chromium brave-bin google-chrome-stable epiphany falkon firefox konqueror midori opera qutebrowser seamonkey surf vivaldi-stable;
+do
+    break
+done
+
+echo "baph -inN $fm $editor $browser"
