ed9a758dcb362765fccef7e3d3df0533e1579241
piotr <nwg.piotr@gmail.com>
2023-01-31 03:03:48 +0100

    update install script

--- webinstaller/arch.sh
+++ webinstaller/arch.sh
@@ -16,21 +16,21 @@ echo "You're about to install the rest of components required for the key bindin
 echo "None of them is a shell dependency, and you're free to change them later."
 
 PS3="Select file manager: "
-select fm in thunar caja dolphin nautilus nemo pcmanfm
+select fm in thunar caja dolphin nautilus nemo pcmanfm;
 do
-    break;
+    break
 done
 
 PS3="Select text editor: "
-select editor in mousepad atom emacs gedit geany kate vim
+select editor in mousepad atom emacs gedit geany kate vim;
 do
-    break;
+    break
 done
 
 PS3="Select web browser: "
-select browser in chromium brave-bin google-chrome-stable epiphany falkon firefox konqueror midori opera qutebrowser seamonkey surf vivaldi-stable
+select browser in chromium brave-bin google-chrome-stable epiphany falkon firefox konqueror midori opera qutebrowser seamonkey surf vivaldi-stable;
 do
-    break;
+    break
 done
 
 baph -inN $fm $editor $browser
