7deeba213d83e0f7418b6fe8b0acd5ae023bdeb1
piotr <nwg.piotr@gmail.com>
2023-01-31 15:29:51 +0100

    improve install script

--- install/arch.sh
+++ install/arch.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/bash
 
+# wget https://raw.github.com/nwg-piotr/nwg-shell/main/installer/arch.sh && chmod u+x arch.sh && ./arch.sh
 sudo pacman -S --noconfirm git man-db vi xdg-user-dirs
-# Set up user dirs
+
+echo Initializing XDG user directories
 xdg-user-dirs-update
-# Let `light` | `brightnessctl` work (needs restart)
+
+echo "Adding $USER to video group"
 sudo usermod -aG video $USER
 
-# Set up Basic AUR Package Helper
+echo "Installing Basic AUR Package Helper"
 git clone https://bitbucket.org/natemaia/baph.git || { echo "Failed cloning baph: $?"; }
 cd baph || { echo "Couldn't setup baph, terminating..."; exit 1; }
 sudo make install
 
-# Install apps
-echo "You're about to install the rest of components required for the key bindings to work."
-echo "None of them is a shell dependency, and you're free to change them later."
+echo "You're about to select a file manager, text editor and web browser."
+echo "They need to be preinstalled now, for the key bindings to work."
+echo "None of above is a shell dependency, and you're free to change them any time later."
 
 PS3="Select file manager: "
 select fm in thunar caja dolphin nautilus nemo pcmanfm;
@@ -33,10 +36,11 @@ do
     break
 done
 
+echo "Installing selection: $fm $editor $browser"
 baph -inN $fm $editor $browser
 
-# Install shell
+echo Installing nwg-shell
 baph -inN nwg-shell
 
-# Run installer (w/o confirmations)
+echo Installing initial configuration
 nwg-shell-installer -w
