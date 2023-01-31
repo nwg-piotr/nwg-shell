a7e2e85beeda005e0eaf7ab1e333634a099dec4e
piotr <nwg.piotr@gmail.com>
2023-01-31 02:55:34 +0100

    update install script

--- webinstaller/arch.sh
+++ webinstaller/arch.sh
@@ -15,22 +15,22 @@ sudo make install
 echo "You're about to install the rest of components required for the key bindings to work."
 echo "None of them is a shell dependency, and you're free to change them later."
 
-PS3="Select file manager: ";
+PS3="Select file manager: "
 select fm in thunar caja dolphin nautilus nemo pcmanfm
 do
-    break
+    break;
 done
 
-PS3="Select text editor: ";
+PS3="Select text editor: "
 select editor in mousepad atom emacs gedit geany kate vim
 do
-    break
+    break;
 done
 
-PS3="Select web browser: ";
+PS3="Select web browser: "
 select browser in chromium brave-bin google-chrome-stable epiphany falkon firefox konqueror midori opera qutebrowser seamonkey surf vivaldi-stable
 do
-    break
+    break;
 done
 
 baph -inN $fm $editor $browser
