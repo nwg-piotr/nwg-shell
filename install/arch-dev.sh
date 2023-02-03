602c430b41989538ac95f08dce384f50ca637bda
jakub <kuba3351@gmail.com>
2023-02-03 18:38:57 +0100

    Fedora install script and other improvements

--- install/arch-dev.sh
+++ install/arch-dev.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
+if [ "$(id -u)" == 0 ] ; then
+   echo "Please don't run this script as root"
+   exit 1
+fi
+
+# Don't continue script if any error occurs.
+set -e
+
 # wget https://raw.github.com/nwg-piotr/nwg-shell/main/install/arch-dev.sh && chmod u+x arch-dev.sh && ./arch-dev.sh && rm ./arch-dev.sh
 sudo pacman -S --noconfirm git man-db vi xdg-user-dirs
 
