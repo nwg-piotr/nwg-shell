270ef380a8142a5b3595593f7b2ffa7d0ec899dd
piotr <nwg.piotr@gmail.com>
2023-06-12 03:39:44 +0200

    improve main help

--- nwg_shell/main.py
+++ nwg_shell/main.py
@@ -6,10 +6,10 @@ from nwg_shell.__about__ import __version__
 def main():
     print("nwg-shell version {}".format(__version__))
     print("Commands:")
-    print("  nwg-shell-installer      installs selected default configs interactively")
-    print("  nwg-shell-installer -a   installs All configs from scratch")
-    print("  nwg-shell-installer -r   Restores missing configs, styles & data files")
-    print("  nwg-shell-check-updates  checks for updates")
+    print("  nwg-shell-installer            installs selected default configs interactively")
+    print("  nwg-shell-installer -a [-hypr] installs All configs from scratch; [-hypr] includes Hyprland configs")
+    print("  nwg-shell-installer -r         Restores missing configs, styles & data files")
+    print("  nwg-shell-check-updates        checks for updates")
     print('\nWiki: https://github.com/nwg-piotr/nwg-shell/wiki')
 
 
