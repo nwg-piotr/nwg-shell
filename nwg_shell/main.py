817bb08abef9f29c9fca0cfa060eeaefc71bd2a8
piotr <nwg.piotr@gmail.com>
2023-08-23 02:10:34 +0200

    update help

--- nwg_shell/main.py
+++ nwg_shell/main.py
@@ -8,8 +8,9 @@ def main():
     print("Commands:")
     print("  nwg-shell-installer            installs selected default configs interactively")
     print("  nwg-shell-installer -a [-hypr] installs All configs from scratch; [-hypr] includes Hyprland configs")
-    print("  nwg-shell-installer -r         Restores missing configs, styles & data files")
-    print("  nwg-shell-check-updates        checks for updates")
+    print("  nwg-shell-installer -w [-hypr] installs configs from scratch w/ no dialogs; [-hypr] includes Hyprland configs")
+    print("  nwg-shell-installer -r [-hypr] Restores missing configs, styles & data files; [-hypr] includes Hyprland configs")
+    print("  nwg-shell-check-updates        system command to check updates; no point in using manually")
     print('\nWiki: https://github.com/nwg-piotr/nwg-shell/wiki')
 
 
