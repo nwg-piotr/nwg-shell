edb3ff0f71b04b341c3eac6630bafc4e096b3602
piotr <nwg.piotr@gmail.com>
2024-08-20 01:56:23 +0200

    fix permissions

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ python3 setup.py install --optimize=1
 install -Dm 644 -t "/usr/share/licenses/nwg-shell" LICENSE
 install -Dm 644 -t "/usr/share/doc/nwg-shell" README.md
 install -Dm 644 -t "/usr/share/applications" nwg-readme.desktop
-install -Dm 644 -t "/usr/bin" scripts/*
+install -Dm 755 -t "/usr/bin" scripts/*
